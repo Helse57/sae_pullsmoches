2d684a9ded075405d401383593f165b6ab04cdf7
THIEBAUT Hugo <hugo.thiebaut6@etu.univ-lorraine.fr>
2023-02-27 22:48:05 +0100

    fonction getDepartements en async

--- www/js/index.js
+++ www/js/index.js
@@ -12,22 +12,35 @@ fetch("https://geo.api.gouv.fr/regions")
                 <div class="card-body">
                     <h5 class="card-title">${region.nom}</h5>
                     <p class="card-text">Code: ${region.code}</p>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-${region.code}">Voir les détails</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-${
+                      region.code
+                    }">Voir les détails</button>
                 </div>
             </div>
     
-            <div class="modal fade" id="modal-${region.code}" tabindex="-1" aria-labelledby="modalLabel-${region.code}" aria-hidden="true">
+            <div class="modal fade" id="modal-${
+              region.code
+            }" tabindex="-1" aria-labelledby="modalLabel-${
+          region.code
+        }" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="modalLabel-${region.code}">${region.nom}</h5>
+                  <h5 class="modal-title" id="modalLabel-${region.code}">${
+          region.nom
+        }</h5>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
                 </div>
                 <div class="modal-body">
                     <p><strong>Code:</strong> ${region.code}</p>
                     <p><strong>Départements:</strong></p>
                     <ul>
-                        ${departements.map((departement) => `<li class="mt-3">${departement.nom} (${departement.code})</li>`).join("")}
+                        ${departements
+                          .map(
+                            (departement) =>
+                              `<li class="mt-3">${departement.nom} (${departement.code})</li>`
+                          )
+                          .join("")}
                     </ul>
                 </div>
               </div>
@@ -54,15 +67,14 @@ champRecherche.addEventListener("input", () => {
   });
 });
 
-function getDepartements(codeRegion) {
-  return fetch(`https://geo.api.gouv.fr/regions/${codeRegion}/departements`)
-    .then((response) => response.json())
-    .then((data) => {
-      const departements = [];
-      data.forEach((dep, index) => {
-        departements.push({ nom: dep.nom, code: dep.code, numero: index + 1 });
-      });
-      return departements;
-    });
+async function getDepartements(codeRegion) {
+  const response = await fetch(
+    `https://geo.api.gouv.fr/regions/${codeRegion}/departements`
+  );
+  const data = await response.json();
+  const departements = [];
+  data.forEach((dep, index) => {
+    departements.push({ nom: dep.nom, code: dep.code, numero: index + 1 });
+  });
+  return departements;
 }
-
