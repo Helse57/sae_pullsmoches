5902637740ae4e627fabac304f41f23720ac3092
THIEBAUT Hugo <hugo.thiebaut6@etu.univ-lorraine.fr>
2023-03-02 14:42:23 +0100

    Fonction THIEBAUT Hugo

--- www/js/index.js
+++ www/js/index.js
@@ -1,30 +1,22 @@
 async function getArticles() {
-  try {
-    const res = await fetch(
-      "https://devweb.iutmetz.univ-lorraine.fr/~thieba218u/sae/sae_pullsmoches/scripts/getArticle.php"
-    );
-    const json = await res.json();
-    return json;
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-async function getDetails(num_art) {
-  try {
-    const res = await fetch(
-      "https://devweb.iutmetz.univ-lorraine.fr/~thieba218u/sae/sae_pullsmoches/scripts/getDetailArticle.php?num_art=" +
-        num_art
-    );
-    const json = await res.json();
-    return json;
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-getArticles().then((data) =>
-  data.forEach((article) =>
-    getDetails(article.num_art).then((test) => console.log(test))
+  return fetch(
+    "https://devweb.iutmetz.univ-lorraine.fr/~thieba218u/sae/sae_pullsmoches/scripts/getArticle.php"
   )
-);
+    .then((response) => response.json())
+    .then((data) => {
+      return Promise.all(data);
+    });
+}
+
+function getDetails(num_art) {
+  return fetch(
+    "https://devweb.iutmetz.univ-lorraine.fr/~thieba218u/sae/sae_pullsmoches/scripts/getDetailArticle.php?num_art=" +
+      num_art
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return Promise.all(data);
+    });
+}
+
+getDetails(1).then((data) => console.log(data));
