28401fc888fae8f9168e7ab5f9a6aa96dee9334e
THIEBAUT Hugo <hugo.thiebaut6@etu.univ-lorraine.fr>
2023-03-09 10:19:12 +0100

    Connexion via cookie

--- www/js/login.js
+++ www/js/login.js
@@ -1,14 +1,21 @@
+const email = "hugothiebaut22@gmail.com";
+const password = "test";
+
 fetch(
   "https://devweb.iutmetz.univ-lorraine.fr/~thieba218u/sae/sae_pullsmoches/scripts/login.php",
   {
     method: "POST",
-    body: JSON.stringify({ username: "Mydao", password: "test" }),
-    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: email, password: password }),
   }
 )
-  .then((response) => {
-    response.json();
-  })
+  .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-  });
+    if (data.length === 1) {
+      console.log("Connexion réussie");
+      document.cookie = `username=${data[0].login}`;
+      document.cookie = `user_id=${data[0].id_cl}`;
+    } else {
+      console.log("Connexion échouée");
+    }
+  })
+  .catch((error) => console.error(error));
